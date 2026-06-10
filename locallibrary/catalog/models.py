6e066d899a757bf157ee27d7aefcd514f68273ae
Gustavo <gustavocoimbra@discente.ufg.br>
2026-06-10 10:40:48 -0300

    update model class author

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -74,4 +74,20 @@ class BookInstance(models.Model):
         return f'{self.id} ({self.book.title})'
 
 
-class 
+class Author(models.Model):
+    """Model representing an author."""
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta: 
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        return f'{self.last_name}, {self.first_name}'
+
+    def get_absolute_url(self):
+        """Return the url to acess a particular author instance."""
+        return reverse('author-detail', args=[str(self.id)])
+    
