d399d139eeb32bbef919c6f0485401b5670e1e1b
Gustavo <gustavocoimbra@discente.ufg.br>
2026-06-10 10:56:32 -0300

    migracao modelos

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -46,10 +46,9 @@ class BookInstance(models.Model):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4,
                       help_text='Unique ID for this particular book across whole library')
-
     book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
-
     imprint = models.CharField(max_length=200)
+    due_back = models.DateField(null=True, blank=True)
 
     LOAN_STATUS = (
         ('m', 'Maintenance'),
