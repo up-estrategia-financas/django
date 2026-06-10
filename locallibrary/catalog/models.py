d21abf88ef2d32c7032e3f1b0973ca3ae3827577
Gustavo Pessoal <gustavocoimbra@discente.ufg.br>
2026-06-10 19:53:40 -0300

    finalizao da home page

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -137,6 +137,7 @@ class BookInstance(models.Model):
 
 class Author(models.Model):
     """Model representing an author."""
+    
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
