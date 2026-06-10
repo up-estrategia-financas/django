6fe5497dfdb122a6e4ce78931d90f3ecbfa662b7
Gustavo <gustavocoimbra@discente.ufg.br>
2026-06-10 12:10:55 -0300

    registrando modelos

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Author, Genre, Book, BookInstance, Language
+
+admin.site.register(Genre)
+admin.site.register(Language)
+admin.site.register(Book)
+admin.site.register(BookInstance)
+admin.site.register(Author)
+
