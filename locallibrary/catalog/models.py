30a547bd16afd99e5c5483809b011f4402bbe19c
Gustavo <gustavocoimbra@discente.ufg.br>
2026-06-10 10:28:04 -0300

    adicionado modelo instancia_livro

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+import uuid
 
 # Create your models here.
 
@@ -35,6 +36,39 @@ class Book(models.Model):
         """String for representing the Model object"""
         return self.title
 
-    def get_absolute(self):
+    def get_absolute_url(self):
         """"Return the url to acess a detail record for this book."""
         return reverse('book-detail', args=[str(self.id)])
+
+    class BookInstance(models.Model):
+        """Model representing a specific copy of a book 
+        (i.e. that can be borrowed from the library)."""
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                          help_text='Unique ID for this particular book across whole library')
+
+    book = models.ForeignKey('Book', on_delete=models.SET_NULL, null=True)
+
+    imprint = models.CharField(max_length=200)
+
+    LOAN_STATUS = (
+        ('m', 'Maintenance'),
+        ('o', 'On loan'),
+        ('a', 'Available'),
+        ('r', 'Reserved'),
+    )
+
+    status = models.CharField(
+        max_length=1,
+        choices=LOAN_STATUS,
+        blank=True,
+        default='m',
+        help_text='Book availability',
+    )
+
+    class Meta:
+        ordering = ['due_back']
+
+    def __str__(self):
+        """String for representing the Model object"""
+        return f'{self.id} ({self.book.title})'
