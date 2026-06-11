fc3cfe7634358c41794556fe5e8781d2e1a3d965
Gustavo <gustavocoimbra@discente.ufg.br>
2026-06-11 12:54:16 -0300

    adicionado testes e forms

--- locallibrary/catalog/forms.py
+++ locallibrary/catalog/forms.py
@@ -2,7 +2,7 @@ import datetime
 
 from django import forms
 from django.core.exceptions import ValidationError
-from django.utils.translation import ugettext_lazyas_
+#from django.utils.translation import ugettext_lazyas_
 
 
 class RenewBookForm(forms.Form):
@@ -14,11 +14,11 @@ class RenewBookForm(forms.Form):
 
         # Check if a data is not in past.
         if data < datetime.date.today():
-            raise ValidationError(_('Invalid date - renewal in past'))
+            raise ValidationError(('Invalid date - renewal in past'))
 
         # Check if a date is in the allowed range (+4 weeks from today).
         if data > datetime.date.today() + datetime.timedelta(weeks=4):
-            raise ValidationError(_('Invalid date - renewal more than 4 weeks ahead'))
+            raise ValidationError(('Invalid date - renewal more than 4 weeks ahead'))
 
         # Remenber to always return the cleaned data.
         return data
