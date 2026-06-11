07176b38826c3203073ec3f68bae13aa10d7f817
Gustavo Pessoal <gustavocoimbra@discente.ufg.br>
2026-06-10 23:14:48 -0300

    adicionado books view

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -5,4 +5,5 @@ from catalog import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('books/', views.BookListView.as_view(), name='books'),
+   # path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
